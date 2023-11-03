cf53f863c3adf19ba2d99b2ff956d44f590b8862
g1dramcs <mr.nikas.style@gmail.com>
2023-11-03 10:18:10 +0500

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:test_screen/preferences/app_colors.dart';
 import 'package:test_screen/widgets/screen.dart';
 import 'package:test_screen/widgets/screen1.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,21 +15,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      // home: MainScreenWidget(),
       theme: ThemeData(
-          appBarTheme: AppBarTheme(backgroundColor: Colors.black87),
+          appBarTheme: AppBarTheme(backgroundColor: AppColorDark.appBarDark),
+          scaffoldBackgroundColor: AppColorDark.mainAppDark,
+          bottomNavigationBarTheme: const BottomNavigationBarThemeData(),
           textTheme:
-              const TextTheme(subtitle1: const TextStyle(color: Colors.white))),
+              const TextTheme(subtitle2: const TextStyle(color: Colors.white))),
       routes: {
-        '/auth': (context) => HomeScreen(),
-        '/next': (context) => MainScreen()
+        '/': (context) => MainScreenWidget(),
       },
-      initialRoute: '/auth',
+      initialRoute: '/',
       onGenerateRoute: (RouteSettings settings) {
         return MaterialPageRoute<void>(builder: (context) {
           return Scaffold(
             body: Center(
-              child: Text("ds"),
+              child: Text("Ошбика перехода меню"),
             ),
           );
         });
--- lib/widgets/screen.dart
+++ lib/widgets/screen.dart
@@ -1,112 +1,34 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  final _emailcontroller = TextEditingController();
-  final _passwordcontroller = TextEditingController();
-  String? errorText = null;
-
-  void _auth() {
-    final login = _emailcontroller.text;
-    final password = _passwordcontroller.text;
-    if (login == "admin" && password == "admin") {
-      errorText = null;
-
-      Navigator.pushNamed(context, '/next');
-    } else {
-      errorText = "Лох, ошибся. *(Джейсон Стэтхем)";
-    }
-
-    setState(() {});
-  }
-
-  final textStyle = const TextStyle(fontSize: 16, color: Colors.white);
-
-  final textFieldDecorator = const InputDecoration(
-      enabledBorder:
-          OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
-      fillColor: Colors.white,
-      labelStyle: TextStyle(color: Colors.white));
+class MainScreenWidget extends StatelessWidget {
+  const MainScreenWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        title: Center(child: Text("Login to your account")),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            children: [
-              SizedBox(height: 20),
-              Text(
-                "Sign in",
-                style: TextStyle(fontSize: 21, color: Colors.white),
-              ),
-              SizedBox(height: 10),
-              Text(
-                "Enter the phone number or email address, and password linked to this account",
-                style: textStyle,
-              ),
-              SizedBox(height: 25),
-              if (errorText != null)
-                Text(
-                  errorText!,
-                  style: TextStyle(color: Colors.red),
-                ),
-              Align(
-                alignment: Alignment.bottomLeft,
-                child: Text(
-                  "Email or Phone",
-                  style: textStyle,
-                ),
-              ),
-              TextField(
-                controller: _emailcontroller,
-                obscureText: false,
-                decoration: textFieldDecorator,
-              ),
-              SizedBox(height: 10),
-              Align(
-                alignment: Alignment.bottomLeft,
-                child: Text(
-                  "Password",
-                  style: textStyle,
-                ),
-              ),
-              TextField(
-                  controller: _passwordcontroller,
-                  obscureText: true,
-                  style: TextStyle(color: Colors.white),
-                  decoration: textFieldDecorator),
-              SizedBox(height: 20),
-              Container(
-                width: double.infinity,
-                height: 50,
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.white)),
-                  onPressed: () {
-                    _auth();
-                  },
-                  child: Text(
-                    "Login",
-                    style: TextStyle(color: Colors.black54, fontSize: 20),
-                  ),
-                ),
-              )
-            ],
-          ),
+        appBar: AppBar(
+          title: Text("Finance"),
+          centerTitle: true,
         ),
-      ),
-    );
+        bottomNavigationBar: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.access_alarm_outlined), label: "text"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.access_alarm_outlined), label: "text"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.mode_standby), label: "text"),
+          ],
+          backgroundColor: Colors.black,
+          selectedLabelStyle: TextStyle(color: Colors.white),
+          unselectedLabelStyle: TextStyle(color: Colors.white),
+          unselectedIconTheme: IconThemeData(
+            color: Colors.white,
+          ),
+          selectedIconTheme: IconThemeData(color: Colors.white),
+        ),
+        body: Column(
+          children: [],
+        ));
   }
 }
